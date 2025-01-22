72aa4d6b848eb6dcbae250e94821c6a1502e044a
ameliadp2002 <amelia.de.paepe@student.howest.be>
2025-01-22 18:08:41 +0100

    added a red circle mask didnt work

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 import './css/reset.css'
 import './css/style.css'
 
+const circleElement = document.querySelector('.circle');
 
+const mouse = { x: 0, y: 0 },
+      circle = { x: 0, y: 0 };
 
+window.addEventListener('mousemove', e => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+const speed = 0.15;
+
+const tick = () => {
+  circle.x += (mouse.x - circle.x) * speed;
+  circle.y += (mouse.y - circle.y) * speed;
+
+  circleElement.style.transform = `translate(${circle.x}px, ${circle.y}px)`;
+
+  window.requestAnimationFrame(tick);
+}
+
+tick();
