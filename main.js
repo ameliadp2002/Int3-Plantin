64c9193efba06a3354d127929ad8f6d76b9e4f88
ameliadp2002 <amelia.de.paepe@student.howest.be>
2025-01-23 23:36:49 +0100

    html finished start of css

--- main.js
+++ main.js
@@ -6,6 +6,38 @@ const circleElement = document.querySelector('.circle');
 const mouse = { x: 0, y: 0 },
       circle = { x: 0, y: 0 };
 
+ const sections = [...document.querySelectorAll("section")];
+const getLinkById = (id) => document.querySelector(`a[href='#${id}']`);
+const inView = (section) => {
+  let top = section.offsetTop;
+  let height = section.offsetHeight;
+
+  while (section.offsetParent) {
+    section = section.offsetParent;
+    top += section.offsetTop;
+  }
+
+  return (
+    top < window.pageYOffset + window.innerHeight &&
+    top + height > window.pageYOffset
+  );
+};
+
+window.onscroll = () => {
+  let next = false;
+
+  sections.forEach((section) => {
+    const a = getLinkById(section.id);
+
+    if (inView(section) && !next) {
+      a.classList.add("nav--current");
+      next = true;
+    } else {
+      a.classList.remove("nav--current");
+    }
+  });
+};
+
 window.addEventListener('mousemove', e => {
   mouse.x = e.x;
   mouse.y = e.y;
